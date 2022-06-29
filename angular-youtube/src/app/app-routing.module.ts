ace71a069a0c92181209d90525eb3d1636006a94
Oleg <ovchinskiy00@mail.ru>
2022-06-29 18:48:47 +0300

    init

---
+++ angular-youtube/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: "/search-page"
+  },
+  {
+    path: "/channel-page"
+  },
+  {
+    path: "/video-page"
+  },
+  {
+    path: "**",
+    redirectTo: "/"
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
