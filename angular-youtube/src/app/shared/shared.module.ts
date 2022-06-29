76be10c95ba367527fe29ef8069158489fc5f08a
Alexander <partizan.pc7@gmail.com>
2022-06-29 19:34:59 +0300

    add shared module

--- angular-youtube/src/app/shared/shared.module.ts
+++ angular-youtube/src/app/shared/shared.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    CommonModule,
-    FormsModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        CommonModule,
+        FormsModule
+    ]
 })
 export class SharedModule { }
 
