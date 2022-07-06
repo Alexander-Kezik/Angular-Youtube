b25c91b67314f4ccf0503c4d3ad9477b9031ae1e
MariaShkrabo <shkrabo.mariya.2000.0473@gmail.com>
2022-07-06 23:24:33 +0300

    AYL-5: add unit tests to the search.service

--- angular-youtube/src/app/search/service/search.service.spec.ts
+++ angular-youtube/src/app/search/service/search.service.spec.ts
@@ -6,9 +6,35 @@ import {
 import { inject } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { of } from 'rxjs';
+import { ICategory } from 'src/app/models/ICategory.interface';
 import { IVideo } from 'src/app/models/IVideo.interface';
 
 import { SearchService } from './search.service';
+
+interface IAPICategory {
+    items: {
+        id: string;
+        snippet: {
+            title: string;
+            assignable: boolean;
+        };
+    };
+}
+
+interface IAPIVideo {
+    items: {
+        id: string;
+        snippet: {
+            title: string;
+            description: string;
+            thumbnails: {
+                high: {
+                    url: string;
+                };
+            };
+        };
+    };
+}
 
 describe('SearchService', () => {
     let service: SearchService;
@@ -24,6 +50,41 @@ describe('SearchService', () => {
         },
     ];
 
+    let CATEGORIES: ICategory[] = [
+        {
+            id: '1',
+            snippet: {
+                title: 'category',
+                assignable: true,
+            },
+        },
+    ];
+
+    let API_CATEGORIES: IAPICategory = {
+        items: {
+            id: '1',
+            snippet: {
+                title: 'category',
+                assignable: true,
+            },
+        },
+    };
+
+    let API_VIDEOS: IAPIVideo = {
+        items: {
+            id: '1',
+            snippet: {
+                title: 'title',
+                description: 'description',
+                thumbnails: {
+                    high: {
+                        url: 'url',
+                    },
+                },
+            },
+        },
+    };
+
     beforeEach(() => {
         httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
         service = new SearchService(httpClientSpy);
@@ -34,10 +95,20 @@ describe('SearchService', () => {
     });
 
     it('should get popular videos', () => {
-        httpClientSpy.get.and.returnValue(of(VIDEOS));
+        httpClientSpy.get.and.returnValue(of(API_VIDEOS));
         service.getPopularVideos().subscribe({
             next: (videos) => {
                 expect(videos).toEqual(VIDEOS);
+            },
+        });
+        expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
+    });
+
+    it('should get categories', () => {
+        httpClientSpy.get.and.returnValue(of(API_CATEGORIES));
+        service.getVideoCategories().subscribe({
+            next: (videos) => {
+                expect(videos).toEqual(CATEGORIES);
             },
         });
         expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
