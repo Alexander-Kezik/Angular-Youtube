e0157f2d292a4003f4ff0caa3366ef351c7a64ef
MariaShkrabo <shkrabo.mariya.2000.0473@gmail.com>
2022-07-06 12:52:44 +0300

    AYL-5: pull changes from develop

--- angular-youtube/src/app/app.module.ts
+++ angular-youtube/src/app/app.module.ts
@@ -4,5 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { SearchModule } from './search/search.module';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+@NgModule({
+    declarations: [AppComponent, HeaderComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule,
+        MatIconModule,
+        SearchModule,
+        FormsModule,
+        MatInputModule,
+        HttpClientModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+})
 export class AppModule {}
